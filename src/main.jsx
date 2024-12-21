de4fb7741563c525dbf2210da6f21fee6a02fe4a
wwwcomcomcomcom <iieiiergn@gmail.com>
2024-12-21 18:41:54 +0900

    fix: wrong spell

--- src/main.jsx
+++ src/main.jsx
@@ -2,13 +2,13 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./App.jsx";
-import { MemoryRouter, Routes } from "react-router-dom";
+import { MemoryRouter, Route, Routes } from "react-router-dom";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
     <MemoryRouter initialEntries={["/"]}>
       <Routes>
-        <Routes path="/" element={<App />} />
+        <Route path="/" element={<App />} />
       </Routes>
     </MemoryRouter>
   </StrictMode>
