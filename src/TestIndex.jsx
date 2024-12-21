dabd397fb43b0f2ade70e62da5ec8b3414d963b2
wwwcomcomcomcom <iieiiergn@gmail.com>
2024-12-21 18:45:50 +0900

    feat: route button to login page

---
+++ src/TestIndex.jsx
@@ -0,0 +1,12 @@
+import { Link } from "react-router-dom";
+
+function TestIndex() {
+  return (
+    <>
+      <h1>Test Module</h1>
+      <Link to="/login">login</Link>
+    </>
+  );
+}
+
+export default TestIndex;
--- src/main.jsx
+++ src/main.jsx
@@ -1,14 +1,14 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
-import App from "./App.jsx";
+import TestIndex from "./TestIndex.jsx";
 import { MemoryRouter, Route, Routes } from "react-router-dom";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
     <MemoryRouter initialEntries={["/"]}>
       <Routes>
-        <Route path="/" element={<App />} />
+        <Route path="/" element={<TestIndex />} />
       </Routes>
     </MemoryRouter>
   </StrictMode>
